0f43bf8e00b8105a346e39ab64dd6248fc8b1ffe
Elena Dergaeva <dergaeva.es@gmail.com>
2018-10-25 01:03:15 +0300

    First commit

--- app.js
+++ app.js
@@ -1,7 +1,58 @@
-function getMockText () {
-    return new Promise(function (resolve, reject){
-        resolve("A year ago I was in the audience at a gathering of designers in San Francisco. There were four designers on stage, and two of them worked for me. I was there to support them. The topic of design responsibility came up, possibly brought up by one of my designers, I honestly don’t remember the details. What I do remember is that at some point in the discussion I raised my hand and suggested, to this group of designers, that modern design problems were very complex. And we ought to need a license to solve them.");
-    })
-}
+document.addEventListener("DOMContentLoaded", () => {
 
-getMockText().then(console.log);
+  const getSelectionText = () => {
+    var text = "";
+    if (window.getSelection) {
+      text = window.getSelection().toString();
+    }
+    return text;
+  };
+  let selection;
+  document.onmouseup = () => {
+    if (getSelectionText()) {
+      selection = getSelectionText();
+    }
+  };
+
+  const replaceSelectionWithHtml = (text, tag) => {
+    var pos = document.getElementsByClassName("nText")[0].innerHTML;
+    const isInTag = pos.search(`<${tag}>${text}</${tag}>`);
+    pos =
+      isInTag === -1
+        ? pos.replace(selection, `<${tag}>${text}</${tag}>`)
+        : pos.replace(`<${tag}>${selection}</${tag}>`, text);
+    document.getElementsByClassName("nText")[0].innerHTML = pos;
+  };
+
+  document.getElementById("bold").onclick = e => {
+    replaceSelectionWithHtml(selection, "b");
+    document.getElementById("bold").classList.toggle("active");
+  };
+
+  document.getElementById("italic").onclick = e => {
+    replaceSelectionWithHtml(selection, "i");
+    document.getElementById("italic").classList.toggle("active");
+  };
+
+  document.getElementById("underline").onclick = e => {
+    replaceSelectionWithHtml(selection, "ins");
+    document.getElementById("underline").classList.toggle("active");
+  };
+
+  /*Related Words*/
+  const words = document.getElementById("file");
+
+  words.addEventListener("mouseover", historyMouseover);
+  function historyMouseover(event) {
+    if(!event.target.hasAttribute("data-tooltip")) {
+      fetch("https://api.datamuse.com/words?rel_syn=" + event.target.textContent)
+        .then(res => res.json())
+        .then(data => data.length ? event.target.setAttribute("data-tooltip", data.slice(0, Math.min(data.length,10)).map(syn => syn.word).join(", ")) : event.target.setAttribute("data-tooltip", "Synonyms Not Found!"))
+        .catch(err => console.log(err));
+    }
+  }
+  /*End Related Words*/
+});
+
+
+
